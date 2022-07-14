3cf5fb56617b44a9b68e5a34195c0bffd265ed88
Matteo Susanna <matteo2784@gmail.com>
2022-07-14 18:13:09 +0200

    Fine esercizio

--- js/main.js
+++ js/main.js
@@ -1,42 +1,48 @@
 const containerDom = document.getElementById('container');
 const hDom = document.getElementById('punteggio');
+let perso = false;
 
+let myArray = [];
 //evento click crea gliglia
 document.getElementById('play').addEventListener('click',
         function(){
 
             containerDom.innerHTML = '';
-            
+                        
             let attiva = 0;
-
+            
+            //reset
+            perso = false;
+            myArray = [];
 
                 for (let i = 1; i <= 100; i++){
 
-                    let casual = numeroRandom( 1, 100);
-                    
+                    let casual = numeriUnici (1, 100);
+ 
                     let element = griglia(casual);
+
                     
                     //evento click box griglia
                     element.addEventListener('click',
                          function(){
-                            
                             //se è divisibile per 6 è una bomba
+                        if(perso == false){
+                            
                             if (casual % 6 == 0){
-                                this.classList.add('bomb')
-                                alert('HAI PERSO il tuo punteggio è di ' + attiva + ' punti')
-                                attiva-- 
+
+                                perso = true;
+                                this.classList.add('bomb');
+                                alert('HAI PERSO il tuo punteggio è di ' + attiva + ' punti');
                             }else {
                                 if(this.classList.contains('normal')){
-                                    attiva--
+                                    attiva--;
                                 }
-                                this.classList.add('normal')
-                                 //punteggio
-                                attiva++
-                                hDom.innerHTML = `<h3>IL PUNTEGGIO E' ${attiva}</h3>`
-                                
-                            }               
+                                this.classList.add('normal');
+                                attiva++;
+                                hDom.innerHTML = `<h3>IL PUNTEGGIO E' ${attiva}</h3>`;
+                            }  
 
-                           
+                        }    
     
                         }
                     );
@@ -51,11 +57,23 @@ function numeroRandom (min, max){
 }
 
 
+function numeriUnici(min, max){
+    let numeroRand = numeroRandom(min, max);
+
+    while(myArray.includes(numeroRand)){
+        numeroRand = numeroRandom(min, max);
+    }
+
+    myArray.push(numeroRand);
+    
+    return numeroRand;
+}
+
 
 
 //funzione per creare la griglia
 
-function griglia (numeroInternoAlBox){
+function griglia(numeroInternoAlBox){
     //creazione box compongono la griglia
     let element = document.createElement('div');
     element.classList.add('box');
@@ -66,7 +84,5 @@ function griglia (numeroInternoAlBox){
     numeroInterno.classList.add('numeroInterno');
     numeroInterno.append(numeroInternoAlBox);
 
-
-    element.append(numeroInterno);
     return element;
 }
